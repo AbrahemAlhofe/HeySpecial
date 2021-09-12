4577a79a5952f87686d40f037a08f1311ca1dc43
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2021-09-12 16:55:56 +0200

    feat( layouts ): add default layout
        - add basic style
        - add main colors

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template lang='pug'>
+    .layout#default
+        Nuxt
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+:root {
+  
+    --white: hsl(0deg, 0%, 100%);
+
+    --primary-95: hsl(200deg, 100%, 95%);
+    --primary-90: hsl(200deg, 100%, 90%);
+    --primary-80: hsl(200deg, 100%, 80%);
+    --primary-70: hsl(200deg, 100%, 70%);
+
+    --gray-100: hsl(0deg, 0%, 98%);
+    --gray-95: hsl(0deg, 0%, 95%);
+    --gray-65: hsl(0deg, 0%, 65%);
+
+    --black: hsl(0deg, 0%, 20%);
+
+}
+* { box-sizing: border-box }
+body { margin: 0; }
+.layout#default {
+    width: 100vw;
+    min-height: 100vh;
+    background-color: var(--gray-95)    
+}
+</style>
