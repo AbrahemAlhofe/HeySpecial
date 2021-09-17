a7c446dbe1e8cd37695c8fbce752ca41e11b056c
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2021-09-17 06:31:54 +0200

    feat( components ): make ( quote ) component responsive
        - sperate ( quote ) component into seperated component file
        - add ( sceen ) mixin to handle screens' media queries

---
+++ components/TheQuote.vue
@@ -0,0 +1,50 @@
+<template lang='pug'>
+   #quote
+    #quote__body
+      | (مش عايز، مش حابب، لا، 🤐،😐) .. شوية تعبيرات يومية لمريض التوحد ، عدم رغبة لفعل اي شيء ، عدم رغبة للتحدث مع الناس او الاختلاط معاهم ، انطوائي .. وحيد ! 
+      | مرض التوحد هو حالة بتأثر علي كيفية تمييز الشخص للناس اللي حواليه وبتغير طريقة تعامله معاهم .. العلاقات البشرية كابوس بالنسباله ! 
+      | وبيأثر علي لغة المريض وسلوكه .. 
+      | دا معناه انك لازم تتعاملي معته معاملة خاصة جداً عشان تقدري ذكاءه .. لأنه غالباً بيكون ذكاء خارق ! ، موقع heyspecial هيقدر يعرفك ازاي تعملي كدة عن طريق مجموعة اسألة وأجوبة .. ، لانه مش مجرد طفل عادي .. هو طفل special ♥️
+    #quote__sign فريق HeySpecial
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='scss'>
+@import '@/styles/mixins/screen.scss';
+#quote {
+
+    width: 95vw;
+    padding: 1em;
+    margin: auto;
+    margin-top: 2.5em;
+    margin-bottom: 4em;
+    position: relative;
+    background-color: var(--white);
+    border-inline: 0.3em solid var(--primary-70);
+    border-radius: 10em 0.5em;
+    box-shadow: 0px 10px 15px var(--gray-90);
+    
+    &__body {
+        text-align: justify;
+        padding: 2em;
+    }
+    
+    &__sign {
+        position: absolute;
+        font-family: 'Pacifico', 'Cairo';
+        left: 1em;
+        bottom: 1em;
+    }
+
+    @include screen("sm") { width: 75vw }
+
+    @include screen("md") { width: 65vw }
+
+    @include screen("xl") { width: 45vw }
+
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <template lang='pug'>
 .page#index
-  .quote
-    .quote__background
-    .quote__body
-      .quote__corner
-      | (مش عايز، مش حابب، لا، 🤐،😐) .. شوية تعبيرات يومية لمريض التوحد ، عدم رغبة لفعل اي شيء ، عدم رغبة للتحدث مع الناس او الاختلاط معاهم ، انطوائي .. وحيد ! 
-      | مرض التوحد هو حالة بتأثر علي كيفية تمييز الشخص للناس اللي حواليه وبتغير طريقة تعامله معاهم .. العلاقات البشرية كابوس بالنسباله ! 
-      | وبيأثر علي لغة المريض وسلوكه .. 
-      | دا معناه انك لازم تتعاملي معته معاملة خاصة جداً عشان تقدري ذكاءه .. لأنه غالباً بيكون ذكاء خارق ! ، موقع heyspecial هيقدر يعرفك ازاي تعملي كدة عن طريق مجموعة اسألة وأجوبة .. ، لانه مش مجرد طفل عادي .. هو طفل special ♥️
-    .quote__sign فريق HeySpecial
+  TheQuote
   .sections
     section.section#tests
       h2.section__title التقييمات
@@ -25,47 +17,6 @@
 export default {}
 </script> 
 <style lang="scss">
-.quote {
-  width: 455px;
-  height: 285px;
-  padding: 1em;
-  margin: auto;
-  margin-top: 2.5em;
-  margin-bottom: 4em;
-  font-family: "Cairo";
-  position: relative;
-  &__corner {
-    shape-outside: ellipse(110px 125px at 32em 20em);
-    width: 100%;
-    height: 100%;
-    float: right;
-  }
-  &__sign {
-    position: absolute;
-    font-family: 'Pacifico', 'Cairo';
-    left: 40px;
-    bottom: -10px;
-  }
-  &__body {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    text-align: justify;
-
-  }
-  &__background {
-    background-color: var(--white);
-    width: 100%;
-    border-right: 0.3em solid var(--primary-70);
-    border-radius: 10em 0.5em;
-    height: 100%;
-    box-shadow: 0px 10px 15px var(--gray-90);
-    transform: translate(3em, 2em);
-  }
-}
-
 .sections {
   display: flex;
   flex-wrap: wrap;
@@ -75,7 +26,6 @@ export default {}
 .section {
 
   &__title {
-    font-family: "Cairo";
     padding-right: 1em;
     color: var(--black);
   }
@@ -95,7 +45,6 @@ export default {}
       -webkit-mask-image: -webkit-gradient(linear, right 75%, left bottom, from(var(--white)), to(hsl(0deg 0% 0% / 0%)));
     }
     &__caption {
-      font-family: 'Cairo';
       flex-grow: 1;
       display: flex;
       align-items: center;
