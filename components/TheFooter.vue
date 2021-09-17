7f0cc90356e76c375152824e4afdb483f2e4ab44
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2021-09-17 08:55:33 +0200

    refactor: enhance facebook link button

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,8 @@
 <template lang='pug'>
   #footer
-    a( href='https://www.facebook.com/Hey-Spacial-102271598877467' ).footer__link#facebook: FacebookIcon
+    .link#facebook
+        .link__text تواصل معنا علي
+        a.link__icon( href='https://www.facebook.com/Hey-Spacial-102271598877467' name='facebook' ): FacebookIcon
 </template>
 
 <script>
@@ -20,20 +22,32 @@ export default {
     background-color: var(--gray-90);
     padding: 1em;
 
-    .footer__link {
-        width: 2em;
+    .link {
         background-color: var(--white);
-        border: none;
         box-shadow: 0px 10px 15px var(--gray-90);
+
         border-radius: 0.5em;
-        font-size: 1.5em;
         cursor: pointer;
-        height: 2em;
         display: inline-flex;
-        justify-content: center;
         align-items: center;
         margin-inline: 0.5em;
         color: var(--black);
+        padding: 0.5em;
+        &__text {
+            font-size: 1.2em;
+        }
+        &__icon {
+            border-radius: 0.5em;
+            width: 2em;
+            height: 2em;
+            background-color: #1877f2;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-right: 0.6em;
+            color: transparent;
+            svg { fill: hsl(0, 0%, 100%) }
+        }
     }
 
 }
