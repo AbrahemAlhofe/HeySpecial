f9569e45395b3df8b9a482fee34d01888f61f415
Tarek Alhofe <alhofetarek11@gmail.com>
2021-09-23 15:21:18 +0200

    fix( pages ): fix games' links

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
 
     IndexSection#games( title='الالعاب' )
       vs-card-group
-        vs-card(v-for='gameTitle, gameName in games' @click='router.push(`/tools/games/${gameName}`)' type='2' )
+        vs-card(v-for='gameTitle, gameName in games' @click='$router.push(`/tools/games/${gameName}`)' type='2' )
           template( #title ): h3 {{ gameTitle }}
           template( #text ): h1
           template( #img ): img( :src='require(`~/assets/images/pages/index/${gameName}.jpg`)' )
