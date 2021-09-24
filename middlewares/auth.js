f849de2642a2b4bec6ff3a805d8b902b48848fd5
mazen saeed <40553471+AhmedSaeed98@users.noreply.github.com>
2021-09-24 18:34:46 +0200

    Login
    Login With Facebook

---
+++ middlewares/auth.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const FacebookStrategy = require("passport-facebook").Strategy;
+var passport = require('passport');
+
+console.log(process.env.FACEBOOK_APP_ID)
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+// Authnitication Request
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  }
+);
+module.exports = app
+
+// connecting with firebase
+// import { FacebookAuthProvider } from "firebase/auth";
+
+
+import { getAuth, signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+const provider = new FacebookAuthProvider();
+// signInWithPopup
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // The signed-in user info.
+    const user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+    const accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+
+    // ...
+  });
