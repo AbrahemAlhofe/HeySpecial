5ab078f2e26e93dfb80bda6afdad27918058f465
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2021-09-25 14:32:24 +0200

    fix( tools ): change Recommendation section's thumbnails extension

--- components/VGame.vue
+++ components/VGame.vue
@@ -32,7 +32,7 @@
                 vs-card(v-for='gameTitle, gameName in games' :key='gameName' @click='$router.push(`/tools/games/${gameName}`)' type='2' )
                     template( #title ): h3 {{ gameTitle }}
                     template( #text ): h1
-                    template( #img ): img( :src='require(`~/assets/images/pages/index/${gameName}.jpg`)' )
+                    template( #img ): img( :src='require(`~/assets/images/pages/index/${gameName}.png`)' )
                     
 
 </template>
